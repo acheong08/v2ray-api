f8047e1dd6fbb07573099c9eb9f4d63dba390a03
Antonio Cheong <acheong@student.dalat.org>
2023-03-06 17:34:15 +0800

    authentication

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"os"
+
 	"github.com/acheong08/v2ray-api/trojan"
 	"github.com/gin-gonic/gin"
 )
+
+func admin_auth(c *gin.Context) {
+	// Get Authorization header
+	auth_header := c.GetHeader("Authorization")
+	// Check if the header matches env variable
+	if auth_header == os.Getenv("ADMIN_AUTH") {
+		c.Next()
+	} else {
+		c.AbortWithStatus(401)
+	}
+}
 
 func main() {
 
@@ -15,12 +28,12 @@ func main() {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
 
-	server.POST("/start", func(c *gin.Context) {
+	server.POST("/admin/start", admin_auth, func(c *gin.Context) {
 		tr.Start()
 		c.JSON(200, gin.H{"message": "started"})
 	})
 
-	server.POST("/stop", func(c *gin.Context) {
+	server.POST("/admin/stop", admin_auth, func(c *gin.Context) {
 		tr.Stop()
 		c.JSON(200, gin.H{"message": "stopped"})
 	})
