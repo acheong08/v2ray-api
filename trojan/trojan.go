c9056175baac26440c9d53e2263f3bf3a566f1d1
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-03-06 18:15:21 +0100

    Improved
    Changes made:
    
    Updated the Start() method to use the -config flag instead of -c to match the server configuration file path.
    Added error handling and appropriate error messages for all methods.
    Fixed the Stop() method to wait for the command to complete before returning.
    Simplified the Restart() method by using Stop() and Start().
    Updated the Status() method to use the kill command with the -0 flag to check if the process is running.
    Updated the GetConfig() method to return an error in case the server configuration file cannot be read.

--- trojan/trojan.go
+++ trojan/trojan.go
@@ -14,12 +14,12 @@ func (trojan *Trojan) Start() error {
 	// Check if ./v2ray exists
 	_, err := exec.LookPath("./v2ray")
 	if err != nil {
-		return err
+		return fmt.Errorf("v2ray binary not found: %w", err)
 	}
-	cmd := exec.Command("./v2ray", "run", "-c", "server.json")
+	cmd := exec.Command("./v2ray", "run", "-config", "server.json")
 	err = cmd.Start()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to start v2ray: %w", err)
 	}
 	// Get the PID
 	trojan.pid = cmd.Process.Pid
@@ -27,46 +27,49 @@ func (trojan *Trojan) Start() error {
 }
 
 func (trojan *Trojan) Stop() error {
-	cmd := exec.Command("kill", "-9", fmt.Sprint(trojan.pid))
+	cmd := exec.Command("kill", fmt.Sprint(trojan.pid))
 	err := cmd.Start()
-	return err
+	if err != nil {
+		return fmt.Errorf("failed to stop v2ray: %w", err)
+	}
+	cmd.Wait()
+	return nil
 }
 
 func (trojan *Trojan) Restart() error {
 	if trojan.Status() == "stopped" {
-		trojan.Start()
-		return nil
+		return trojan.Start()
 	}
-	err := trojan.Stop()
-	if err != nil {
-		return err
+	if err := trojan.Stop(); err != nil {
+		return fmt.Errorf("failed to stop v2ray: %w", err)
 	}
-	trojan.Start()
-	return err
+	return trojan.Start()
 }
 
 func (trojan *Trojan) Status() string {
 	// Check if PID is running
-	cmd := exec.Command("ps", "-p", fmt.Sprint(trojan.pid))
+	cmd := exec.Command("kill", "-0", fmt.Sprint(trojan.pid))
 	err := cmd.Run()
 	if err != nil {
 		return "stopped"
-	} else {
-		return "running"
 	}
+	return "running"
 }
 
 func (trojan *Trojan) Configure(config string) error {
 	// Write config to server.json
 	err := os.WriteFile("server.json", []byte(config), 0644)
-	return err
+	if err != nil {
+		return fmt.Errorf("failed to write to server.json: %w", err)
+	}
+	return nil
 }
 
-func (trojan *Trojan) GetConfig() string {
+func (trojan *Trojan) GetConfig() (string, error) {
 	// Read config from server.json
 	config, err := os.ReadFile("server.json")
 	if err != nil {
-		return ""
+		return "", fmt.Errorf("failed to read server.json: %w", err)
 	}
-	return string(config)
+	return string(config), nil
 }
