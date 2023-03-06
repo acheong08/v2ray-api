9512ccad27e81a5a0b6a97a8406db6a1534e2483
Antonio Cheong <acheong@student.dalat.org>
2023-03-06 17:32:28 +0800

    give up on direct access

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-
 	"github.com/acheong08/v2ray-api/trojan"
 	"github.com/gin-gonic/gin"
 )
 
-var trojan_server *trojan.Trojan
-
-func init() {
-	trojan_server = trojan.New()
-}
-
 func main() {
+
+	tr := trojan.Trojan{}
 
 	server := gin.Default()
 
@@ -21,49 +15,17 @@ func main() {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
 
-	server.POST("/restart", restart)
+	server.POST("/start", func(c *gin.Context) {
+		tr.Start()
+		c.JSON(200, gin.H{"message": "started"})
+	})
 
-	server.POST("/stop", stop)
+	server.POST("/stop", func(c *gin.Context) {
+		tr.Stop()
+		c.JSON(200, gin.H{"message": "stopped"})
+	})
 
 	// Run
 	server.Run(":8080")
 
-}
-
-type Config struct {
-	Inbounds  []interface{} `json:"inbounds"`
-	Outbounds []interface{} `json:"outbounds"`
-}
-
-// Handlers
-func restart(c *gin.Context) {
-	// Get JSON config
-	var config Config
-	err := c.BindJSON(&config)
-	if err != nil {
-		c.JSON(400, gin.H{"message": "Bad request"})
-		return
-	}
-	config_string, _ := json.Marshal(config)
-	// Create trojan server
-	if trojan_server.Status() == "exists" {
-		err = trojan_server.RestartWithNewConfig(string(config_string))
-	} else {
-		err = trojan_server.CreateAndRun(string(config_string))
-
-	}
-	if err != nil {
-		c.JSON(500, gin.H{"message": "Internal server error", "error": err.Error()})
-		return
-	}
-	c.JSON(200, gin.H{"message": "Restarted"})
-}
-
-func stop(c *gin.Context) {
-	err := trojan_server.Stop()
-	if err != nil {
-		c.JSON(500, gin.H{"message": "Internal server error"})
-		return
-	}
-	c.JSON(200, gin.H{"message": "Stopped"})
 }
--- trojan/trojan.go
+++ trojan/trojan.go
@@ -1,113 +1,22 @@
 package trojan
 
 import (
-	"encoding/json"
-	"io"
-	"strings"
-
-	"github.com/v2fly/v2ray-core/v5/common"
-
-	v2ray "github.com/v2fly/v2ray-core/v5"
+	"fmt"
+	"os/exec"
 )
 
-func init() {
-	// Register JSON loader
-	common.Must(v2ray.RegisterConfigLoader(&v2ray.ConfigFormat{
-		Name:      []string{"json"},
-		Extension: []string{"json"},
-		Loader: func(input interface{}) (*v2ray.Config, error) {
-			config := new(v2ray.Config)
-			switch v := input.(type) {
-			case string:
-				err := json.Unmarshal([]byte(v), config)
-				if err != nil {
-					return nil, err
-				}
-				return config, nil
-			case []byte:
-				err := json.Unmarshal(v, config)
-				if err != nil {
-					return nil, err
-				}
-			case io.Reader:
-				err := json.NewDecoder(v).Decode(config)
-				if err != nil {
-					return nil, err
-				}
-			}
-			return config, nil
-		},
-	}))
-}
-
 type Trojan struct {
-	instance *v2ray.Instance
+	pid int
 }
 
-func (trojan *Trojan) Create(raw_config string) error {
-	// Create io.Reader from raw_config
-	config, err := v2ray.LoadConfig("json", strings.NewReader(raw_config))
-	if err != nil {
-		return err
-	}
-	trojan.instance, err = v2ray.New(config)
-	if err != nil {
-		return err
-	}
-	return nil
+func (trojan *Trojan) Start() {
+	cmd := exec.Command("v2ray", "run", "-c", "server.json")
+	cmd.Start()
+	// Get the PID
+	trojan.pid = cmd.Process.Pid
 }
 
-func (trojan *Trojan) CreateAndRun(raw_config string) error {
-	err := trojan.Create(raw_config)
-	if err != nil {
-		return err
-	}
-	err = trojan.Start()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (trojan *Trojan) Start() error {
-	err := trojan.instance.Start()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (trojan *Trojan) Stop() error {
-	err := trojan.instance.Close()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (trojan *Trojan) RestartWithNewConfig(raw_config string) error {
-	err := trojan.Stop()
-	if err != nil {
-		return err
-	}
-	err = trojan.Create(raw_config)
-	if err != nil {
-		return err
-	}
-	err = trojan.Start()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (trojan *Trojan) Status() string {
-	if trojan.instance == nil {
-		return "nil"
-	}
-	return "exists"
-}
-
-func New() *Trojan {
-	return &Trojan{}
+func (trojan *Trojan) Stop() {
+	cmd := exec.Command("kill", "-9", fmt.Sprint(trojan.pid))
+	cmd.Start()
 }
